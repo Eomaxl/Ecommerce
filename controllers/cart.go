61ecfaea5cb49fc3c500f9c61210d0609fb35f79
Eomaxl <souravsnigdh@gmail.com>
2025-04-16 22:51:57 +0530

    Implementing Cart methods - inprogress

--- controllers/cart.go
+++ controllers/cart.go
@@ -1,1 +1,25 @@
 package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func AddToCard() gin.Handler {
+
+}
+
+func RemoveItem() gin.HandlerFunc {
+
+}
+
+func GetItemFromCart() gin.HandlerFunc {
+
+}
+
+func BuyFromCart() gin.HandlerFunc {
+
+}
+
+func InstantBuy() gin.HandlerFunc {
+
+}
--- database/cart.go
+++ database/cart.go
@@ -1,1 +1,23 @@
-package database
+package controllers
+
+import (
+	"errors"
+)
+
+var (
+	ErrCantFindProduct    = errors.New("Can't find product")
+	ErrCantDecodeProducts = errors.New("Can't decode products")
+	ErrUserIdIsNotValid   = errors.New("UserId is not valid")
+	ErrCantUpdateUser     = errors.New("Can't update user")
+	ErrCantRemoveItemCart = errors.New("Can't remove item cart")
+	ErrCantGetItem        = errors.New("Can't get item")
+	ErrCantBuyCartItem    = errors.New("Can't buy cart item")
+)
+
+func AddProductToCart() {}
+
+func RemoveCartItem() {}
+
+func BuyItemFromCart() {}
+
+func InstantBuyer() {}
